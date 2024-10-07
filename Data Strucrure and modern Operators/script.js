d631eec79efb78c4db0e4863a262e32d82887d55
RAMESHYADAVBCA <ry275886@gmail.com>
2024-10-07 20:40:37 +0530

    initial code

--- Data Strucrure and modern Operators/script.js
+++ Data Strucrure and modern Operators/script.js
@@ -195,7 +195,7 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = "Ristorante Roma";
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
-*/
+
 
 // Rest Pattern and Parameters
 
@@ -279,3 +279,120 @@ add(...x);
 
 restaurant.orderPizza("mushroom", "onion", "olives", "spinach");
 restaurant.orderPizza("mushroom");
+ */
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starteMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Capress Salad"],
+  mainMenu: ["Pizaa", "Pasta", "Risotto"],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 12,
+      close: 22,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starteMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = 20.0,
+    address,
+  }) {
+    console.log(`order received! ${this.starteMenu[starterIndex]} and ${this.mainMenu[mainIndex]} 
+      will be delivered to ${address} at ${time}`);
+  },
+  orderPasta: function (ing1, ing2, ing2) {
+    console.log(`Here is your declicious pasta with ${ing1}, $
+      {ing2} and ${ing3}`);
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+//110- LOGICAL ASSIGNMENT OPERATORS
+const rest1 = {
+  name: "Capri",
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: "La Piazza",
+  owner: "Giovanni Rossi",
+};
+
+// OR assignment operator
+
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;
+// rest1.numGuests ||= 10;
+
+// nullish assignment operator(null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+// AND assignment operator
+// rest1.owner = rest1.owner && "<ANONYMOUS>";
+// rest2.owner = rest2.owner && "<ANONYMOUS>";
+
+rest1.owner &&= "<ANONYMOUS>";
+rest2.owner &&= "<ANONYMOUS>";
+
+console.log(rest1);
+console.log(rest2);
+
+/*
+// THE NULLISH COALESCING OPERATOR(??)
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+
+/*
+// Short Circuiting (&& and ||)
+console.log("----OR----");
+// Use AND data type, retyen ANY data type, short-circuiting
+console.log(3 || "Jonas");
+console.log("" || "Jonas");
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || "" || "Hello" || 23 || null);
+
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log("----AND----");
+console.log(0 && "Jonas");
+console.log(7 && "Jonas");
+
+console.log("Hello" && 23 && null && "jonas");
+
+if (restaurant.orderPizza) {
+  restaurant.orderPizza("mushroom", "spinach");
+}
+restaurant.orderPizza && restaurant.orderPizza("mushroom", "spinach");
+*/
