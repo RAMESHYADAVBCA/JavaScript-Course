f64e0cb7b61cf31db9a404526bec68da7bc8526e
RAMESHYADAVBCA <ry275886@gmail.com>
2024-10-10 21:24:27 +0530

    initial code

--- Data Strucrure and modern Operators/script.js
+++ Data Strucrure and modern Operators/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 // ENHANCED OBJECT LITERALS
 const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 const openingHours = {
@@ -49,6 +49,99 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+*/
+
+// CODING CHALLENGE #2
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1.
+for (const [i, player] of game.scored.entries())
+  console.log(`Goal ${i + 1}: ${player}`);
+
+// 2.
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) average += odd;
+average /= odds.length;
+console.log(average);
+
+// 3.
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr =
+    team === "x"
+      ? "draw"
+      : `victory
+  & {game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+}
+/*
+////////////////////////////////////
+//  LOOPING OBJECTS: OBJECT KEYS. VALUES AND ENTERS
+
+// Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `we are open on ${properties.length} days:`;
+
+for (const day of properties) {
+  openStr += `${day},`;
+}
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entrire Object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// [key values]
+for (const [day, { open, close }] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
+
+
 
 // OPTIONAL CHAINING(?.)
 
