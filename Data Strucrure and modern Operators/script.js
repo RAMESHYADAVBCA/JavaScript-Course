1260a6d78db7db01791517ac78d1ba48b8fc5445
RAMESHYADAVBCA <ry275886@gmail.com>
2024-10-11 17:24:13 +0530

    initial code

--- Data Strucrure and modern Operators/script.js
+++ Data Strucrure and modern Operators/script.js
@@ -1,5 +1,5 @@
 "use strict";
-/*
+
 // ENHANCED OBJECT LITERALS
 const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 const openingHours = {
@@ -49,8 +49,146 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
-*/
 
+// SUMMARY: WHICH DATA STRUCTURE TO USE?
+// CHALLENGE#3
+const gameEvents = new Map([
+  [17, "GOAL"],
+  [36, "Substitution"],
+  [47, "GOAL"],
+  [61, "Substitution"],
+  [64, "Yello card"],
+  [69, "Red card"],
+  [70, "Substitution"],
+  [72, "Substitution"],
+  [76, "GOAL"],
+  [80, "GOAL"],
+  [92, "🔸Yellow card"],
+]);
+
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+
+// 3.
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+// 4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? "FIRST" : "SECOND";
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
+
+/*
+///////////////////////
+// Maps: iteration
+const question = new Map([
+  ["question", "what is the programming language in the world"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct"],
+  [false, "Try again"],
+]);
+console.log(question);
+
+//Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get("question"));
+for (const [key, value] of question) {
+  if (typeof key === "number") console.log(`Answer ${key}. ${value}`);
+}
+const answer = Number(prompt("Your answer"));
+console.log(answer);
+
+console.log(question.get(question.get("correct") === answer));
+
+//Convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+//
+
+/*
+////////////////////////
+// MAPS: Fundamentals
+const rest = new Map();
+rest.set("name", "Classico Itraliano");
+rest.set(1, "Firenze", "Italy");
+console.log(rest.set(2, "Lisbon, prortugal"));
+
+rest
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23)
+  .set(true, "We are open :D")
+  .set(false, "We are closed :(");
+
+console.log(rest.get("name"));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+console.log(rest.has("categories"));
+rest.delete(2);
+// rest.clear();
+const arr = [1, 2];
+rest.set(arr, "Test");
+rest.set(document.querySelector("h1"), "Heading");
+console.log(rest);
+console.log(rest.size);
+console.log(rest.get(arr));
+
+
+//////////////////////////
+// SETS
+const ordersSet = new Set([
+  "Pasta",
+  "Pizza",
+  "Pizza",
+  "Risotto",
+  "Pasta",
+  "Pizza",
+]);
+console.log(ordersSet);
+
+console.log(new Set("Jonas"));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has("Pizza"));
+console.log(ordersSet.has("Bread"));
+ordersSet.add("Garlic Bread");
+ordersSet.delete("Risotto");
+// ordersSet.clear();
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+// Example
+const staff = ["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(
+  new Set(["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"]).size
+);
+
+console.log(new Set("RameshYadav").size);
+
+/*
+/////////////////////////
 // CODING CHALLENGE #2
 const game = {
   team1: "Bayern Munich",
@@ -113,6 +251,7 @@ for (const [team, odd] of Object.entries(game.odds)) {
   & {game[team]}`;
   console.log(`Odd of ${teamStr} ${odd}`);
 }
+
 /*
 ////////////////////////////////////
 //  LOOPING OBJECTS: OBJECT KEYS. VALUES AND ENTERS
