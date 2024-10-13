54648e24cc36e5e60eec93987bf0a364e5d42351
RAMESHYADAVBCA <ry275886@gmail.com>
2024-10-13 20:28:57 +0530

    initial code

--- A Closer LooK at Functions/script.js
+++ A Closer LooK at Functions/script.js
@@ -112,19 +112,23 @@ greet("Hello")("Jonas");
 const greetArr = (greeting) => (name) => console.log(`${greeting} ${name}`);
 
 greetArr("Hi")("Jonas");
-*/
+
 
 ///////////////////////////
 // THE CALL AND APPLY METHODS
 const lufthansa = {
   airline: "Lufthansa",
   iataCode: "LH",
-  booking: [],
+  bookings: [],
   //booking: function(){}
   book(flightNum, name) {
     console.log(
       `${name}booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
     );
+    this.bookings.push({
+      flight: `${this.iataCode}${this.iataCode}${flightNum}`,
+      name,
+    });
   },
 };
 
@@ -137,3 +141,181 @@ const eurowings = {
   iataCode: "EW",
   bookings: [],
 };
+
+const book = lufthansa.book;
+// Does NOT work
+// book(23, 'Sarah Williams');
+
+// Call method
+book.call(eurowings, 23, "Sarah Williams");
+console.log(eurowings);
+
+book.call(lufthansa, 239, "Mary Cooper");
+console.log(lufthansa);
+
+const swiss = {
+  airline: "Swiss Air Lines",
+  iataCode: "LX",
+  bookings: [],
+};
+
+book.call(swiss, 583, "Mary Cooper");
+console.log(swiss);
+
+// Apply method
+const flightData = [583, "George Cooper"];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
+
+//Bind method
+//book.call(eurowings, 23, "Sarah Williams");
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, "Steven Williams");
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23("Jonas Schmedtmann");
+bookEW23("Martha Cooper");
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+lufthansa.buyPlane();
+// document
+// .querySelector(".buy")
+// .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+
+const addTax = (rate, value) => value + value + rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+//addVAT = (value) => value + value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
+
+/////////////////
+// CHALLENGE#1
+const poll = {
+  question: "What is your favourite programming language?",
+  options: ["0:JavaScript", "1: Python", "2: Rust", "3: C++"],
+  // This generate[0, 0, 0, 0].More in the next section
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // Get answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join("\n")}\n(Write option number)`
+      )
+    );
+    console.log(answer);
+
+    // Register answer
+    typeof answer === "number" &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+    this.displayResults();
+    this.displayResults("string");
+  },
+  displayResults(type = "array") {
+    if (type === "array") {
+      console.log(this.answers);
+    } else if (type === "string") {
+      //poll results are 13, 2, 4, 1
+      console.log(`Poll results are ${this.answers.join(",")}`);
+    }
+  },
+};
+// poll.registerNewAnswer();
+
+document
+  .querySelector(".poll")
+  .addEventListener("click", poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answer: [5, 2, 3] }, "string");
+
+
+///////////////////
+// IMMEDIATELY INVOKED FUNCTION EXPRESSIONS(IIFE)
+const runOnce = function () {
+  console.log("This will never run again");
+};
+runOnce();
+// ITFE
+(function () {
+  console.log("This will never run again");
+  const isPrivate = 23;
+})();
+
+//console.log(isPrivate);
+
+(() => console.log("This will ALSO never run again"))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+// console.log(isPrivate);
+console.log(notPrivate);
+
+///////////////////
+// More Closure Examples
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+g();
+f();
+console.dir(f);
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPasssengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, (wait = 1000));
+  console.log(`Will start boarding in ${wait} second`);
+};
+
+const perGroup = 1000;
+boardPasssengers(180, 3);
+*/
