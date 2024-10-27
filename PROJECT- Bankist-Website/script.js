a920a4896e7e02484e25eb9c7abd846429ce3fbc
RAMESHYADAVBCA <ry275886@gmail.com>
2024-10-27 19:59:30 +0530

    initial code

--- PROJECT- Bankist-Website/script.js
+++ PROJECT- Bankist-Website/script.js
@@ -10,7 +10,192 @@ const nav = document.querySelector(".nav");
 const tabs = document.querySelectorAll(".operations__tab");
 const tabsContainer = document.querySelector(".operations__tab-container");
 const tabsContent = document.querySelectorAll(".operations__Content");
-//////////////////////
+
+const OpenModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const CloseModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", OpenModal));
+btnCloseModal.addEventListener("click", CloseModal);
+overlay.addEventListener("click", CloseModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+/////////////////////////
+
+// Button scrolling
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(e.target.getBoundingClientRect());
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log("Current scroll (X/Y)", window.pageXOffset, pageYOffset);
+
+  console.log(
+    "height/width viewport",
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(s1coords.left + window.pageYOffset,
+  //   s1coords.top  + window.pageYOffset);
+
+  // window.scrollTo({left: s1coords.left + window.pageYOffset,
+  //   top: s1coords.top  + window.pageYOffset,
+  // behavior: 'smooth',});
+
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+///////////////////////////
+// Page navigation
+
+// document.querySelectorAll(".nav_link").forEach(function (e) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+//    });
+// });
+
+// 1.Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector(".nav_link").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav_link")) {
+    const id = this.getAttribute("href");
+    console.log(id);
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+});
+
+// BUILDING A TABBED COMPONENT
+const tabs1 = document.querySelectorAll(".operations_tab");
+const tabsContainer2 = document.querySelector(".operations_tab-container");
+const tabsContainer3 = document.querySelectorAll(".operations_content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations_tab");
+  console.log(clicked);
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove Active classes
+  tabs.forEach((t) => t.classList.remove("operations_tab--active"));
+  clicked.classList.add("operations_tab--active");
+  tabsContent.forEach((c) => c.classList.remove("operations_content--active"));
+
+  // Activate tab
+  clicked.classList.add("operations_tab--active");
+
+  // Activate content area
+  document
+    .querySelector(`.operations_content--${clicked.dataset.tab}`)
+    .classList.add("operations_contant--active");
+});
+
+// Menu fade animation
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains(".nav_link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelector(".nav_link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// passing "argument" into handler
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+
+nav.addEventListener("mouseout", handleHover.bind(1));
+//////////////////////////////////
+// Sticky navigation
+// const initialCoords = section1.getBoundingClientRect();
+// console.log(initialCoords);
+
+// window.addEventListener("scroll", function () {
+//   console.log(window.scrollY);
+
+//   if (window.scrollY > initialCoords.top) nav.classList.add("sticky");
+//   else nav.classList.remove("sticky");
+// });
+
+// Sticky navigation: Intersection Observer API
+
+const obsCallBack = function (entries, observer) {
+  entries.forEach((entry) => {
+    console.log(entry);
+  });
+};
+
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersection) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+
+// Reveal sections
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
+/*
 /////////////////////
 
 // selecting elements
@@ -128,7 +313,7 @@ btnScrollTo.addEventListener("click", function (e) {
 
   section1.scrollIntoView({ behavior: "smooth" });
 });
-
+///////////////////////////////
 // Types of Events and Events Handlers
 
 const h1 = document.querySelector("h1");
@@ -143,10 +328,63 @@ setTimeout(()=> h1.removeEventListener('mouseenter', alertH1), 3000);
 // h1.onmouseenter = function (e) {
 //   alert("onmouseenter: Great! You are reading the heading :D");
 // };
-*/
 
 // Event Propagation in Practice
 // rgb(255, 255, 255)
 const randomInt = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
-const randomColor = () => ``;
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector(".nav_link").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log("LINK", e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // Stop propagation
+  // e.stopPropagation();
+});
+
+document.querySelector(".nav_link").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log("CONTAINER", e.target, e.currentTarget);
+});
+
+document.querySelector(".nav").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log("NAV", e.target, e.currentTarget);
+});
+
+
+// DOM Traversing
+
+const h1 = document.querySelector("h1");
+
+// Going downwards: child
+console.log(h1.querySelectorAll(".highlight"));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "orangered";
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest(".header").Style.background = "var(--gradient-secondary)";
+h1.closest("h1").Style.background = "var(--gradient-primary)";
+
+//  Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = "scale(0.5)";
+});
+*/
+
+////////////////////////////////
