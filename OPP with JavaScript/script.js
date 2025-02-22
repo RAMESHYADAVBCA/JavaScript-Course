4af9cb200c0f9a6ecbd87f29f49de5acad9fea91
RAMESHYADAVBCA <ry275886@gmail.com>
2025-02-22 22:08:32 +0530

    add code

--- OPP with JavaScript/script.js
+++ OPP with JavaScript/script.js
@@ -123,11 +123,28 @@ class PersonCl {
   greet() {
     console.log(`Hey ${this.firstName}`);
   }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(" ")) this.fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
 const jessica = new PersonCl("Jessica", 1996);
 console.log(jessica);
 jessica.calcAge();
+
+console.log(jessica.age);
 
 console.log(jessica.__proto__ === PersonCl.prototype);
 
@@ -139,3 +156,23 @@ jessica.greet();
 // 1. classes are NOT hoisted
 // 2. Classses are first-class citizes
 // 3. classes are executed in strict mode
+
+const walter = new PersonCl("Walter white", 1995);
+
+const account = {
+  owner: "Ramesh",
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
